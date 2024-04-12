c66deeffb54ff386ee9f0a6eefc98523bf919373
Sherin saji <146155179+sherinsaji126@users.noreply.github.com>
2024-04-12 21:00:47 +0530

    Pattern_8

---
+++ Interviw_Pattern_Programs/Alphabet_Palindrome_Triangle.cpp
@@ -0,0 +1,31 @@
+/*
+        A 
+      A B A
+    A B C B A
+  A B C D C B A
+A B C D E D C B A
+*/
+#include<iostream>
+using namespace std;
+int main(){
+
+int num;
+
+cout<<"Enter The Number Of Rows: ";
+cin>>num;
+
+for(int k=1;k<=num;k++){
+for(int i=0;i<2*(num-k);i++){
+    cout<<" ";
+}
+
+for(int j=1;j<=k;j++){
+    cout<<char(j+64)<<" ";
+}
+for(int j=k-1;j>0;j--){
+    cout<<char(j+64)<<" ";
+}
+cout<<endl;
+}
+
+}
